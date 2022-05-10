7ea5482113104b7619c9e9e99249be3af9468242
ibrahimyildiz11 <ibrahimyildiz@protonmail.com>
2022-05-10 10:37:12 +0000

    profiles

--- src/main/java/be/vdab/profiles/ProfilesApplication.java
+++ src/main/java/be/vdab/profiles/ProfilesApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProfilesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProfilesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilesApplication.class, args);
+    }
 
 }
--- src/test/java/be/vdab/profiles/repositories/DataSourceTest.java
+++ src/test/java/be/vdab/profiles/repositories/DataSourceTest.java
@@ -1,2 +1,22 @@
-package be.vdab.profiles.repositories;public class DataSourceTest {
+package be.vdab.profiles.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+@DataJpaTest
+class DataSourceTest {
+    private final DataSource dataSource;
+
+    DataSourceTest(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Test
+    void databaseNaam() throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            System.out.println(connection.getCatalog());
+        }
+    }
 }
